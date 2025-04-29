76612fd1ae81317f8ec77620bac631fe2c267e9f
Gaurav-Indoriya <gaurav.indoriya82@gmail.com>
2025-04-29 11:43:58 +0530

    animations & about

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,31 @@
 const jwt = require('jsonwebtoken');
-const adminModel = require('../model/admin'); //importing admin model
+const adminModel = require('../model/admin'); // importing admin model
 
+const checkAuth = async (req, res, next) => {
+    const token = req.cookies.token;
 
-const checkAuth = async (req, res, next) => { //middleware to check authentication
-    const token = req.cookies.token; //getting token from cookie
-    if (!token){
+    if (!token) {
         req.flash('LoginError', 'Unauthorized! Please login first');
         return res.redirect('/login');
     }
+
     try {
         const verifyToken = jwt.verify(token, 'gaurav$');
-        const user = await adminModel.findOne({where : { ID: verifyToken.ID }});
+        const user = await adminModel.findOne({ where: { ID: verifyToken.ID } });
+
         if (user) {
             req.admin = user;
+            //Prevent browser from caching this authenticated page
+            res.set('Cache-Control', 'no-store');
             next();
         } else {
             req.flash('LoginError', 'Session expired or invalid token, please login again!');
             res.redirect('/login');
         }
     } catch (error) {
-        
+        req.flash('LoginError', 'Invalid token or session expired!');
+        res.redirect('/login');
     }
-}
+};
 
-module.exports = checkAuth; //exporting authentication middleware
+module.exports = checkAuth;
